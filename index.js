2841256ae56e88bcaa20cded2a71f21cb5219295
Oling Emmanuel Benjamin <olingemmanuel5@gmail.com>
2023-09-01 03:58:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+app.post("/post", (req, res) => {
+  res.json(req.body);
+});
+
+app.listen(3000, () => {
+  console.log("the server is running at port 3000");
+});
